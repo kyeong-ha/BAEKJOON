0f182be55105ddfca837752f2172b65b250e1f27
KyeongHa <rudgk030@hanyang.ac.kr>
2023-03-19 21:54:13 +0900

    2023-03-19

--- CLASS3/1676.js
+++ CLASS3/1676.js
@@ -1,4 +1,16 @@
 // 팩토리얼 0의 개수
 const input = Number(require('fs').readFileSync('test.txt'));
 
-console.log(input)
+function factorial(n){
+    if(n==1 || n==0) return 1n;
+    return BigInt(n)*BigInt(factorial(n-1));
+}
+
+const number = factorial(input).toString();
+let number_of_zero = 0;
+
+for(let i=number.length-1; i>=0; i--){
+    if(number[i] == 0) number_of_zero++;
+    else break;
+}
+console.log(number_of_zero)
